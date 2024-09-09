bab943a5413460d6d66f05fbca2effeda7cc835c
VinayMandhaan <vinaymandhaan29@gmail.com>
2024-09-10 00:33:09 +0400

    Basket UI Fix

--- src/components/Menu/Basket/index.tsx
+++ src/components/Menu/Basket/index.tsx
@@ -9,9 +9,9 @@ interface BasketProps {
 
 const Basket: React.FC<BasketProps> = ({ cart }) => {
     return (
-        <div className="flex items-center justify-center w-[36px] h-[36px] relative">
+        <div className="flex items-center justify-center w-[50px] h-[36px] relative">
             <div className="absolute top-0 right-0">
-                <Heading title={cart?.reduce((value, current) => Number(value) + Number(current.quantity), 0)} style={'text-[14px]'} />
+                <Heading title={cart?.reduce((value, current) => Number(value) + Number(current.quantity), 0)} style={'text-[12px]'} />
             </div>
             <Image url={BasketImage} alt="basket-icon" style="w-[20px] h-[20px]" />
         </div>
